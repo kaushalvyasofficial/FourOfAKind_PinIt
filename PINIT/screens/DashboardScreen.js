033d50195c4f5f49da717a83dbca467831222788
Gourav2609 <herculeswarrior.in@gmail.com>
2023-10-27 03:31:04 +0530

    Added Tab navigation

--- PINIT/screens/DashboardScreen.js
+++ PINIT/screens/DashboardScreen.js
@@ -10,9 +10,10 @@ import {
 } from "react-native";
 
 import HomeLogo from "../assets/images/home";
-import EventLogo from '../assets/images/Calendar'
-import SearchLogo from '../assets/images/search'
-import ProfileLogo from '../assets/images/profile'
+import EventLogo from "../assets/images/Calendar";
+import SearchLogo from "../assets/images/search";
+import ProfileLogo from "../assets/images/profile";
+import FloatingButton from "./component/FloatingButton";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -51,44 +52,58 @@ const ProfileScreen = () => {
 };
 
 const DashboardScreen = ({ navigation }) => {
-  console.log("DashboardScreen");
+  //   console.log("DashboardScreen");
   return (
-    <Tab.Navigator>
-      <Tab.Screen 
-            name="Home" 
-            component={HomeScreen}
-            options={{
-                tabBarIcon: ({ color, size }) => (
-                    <HomeLogo color='#635bff' size='50' />
-                ),
-            }} 
+    <Tab.Navigator
+      tabBarOprions={{
+        activeTintColor: "white",
+        inactiveTintColor: "black",
+        showLabel: false,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <HomeLogo color="#635bff" size="50" />
+          ),
+        }}
       />
-      <Tab.Screen 
-            name="Event" 
-            component={EventScreen}
-            options={{
-                tabBarIcon: ({ color, size }) => (
-                    <EventLogo color='#635bff' />
-                ),
-            }}
+      <Tab.Screen
+        name="Event"
+        component={EventScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <EventLogo color="#635bff" />,
+        }}
       />
-      <Tab.Screen 
-            name="Search" 
-            component={SearchScreen}
-            options={{
-                tabBarIcon: ({ color, size }) => (
-                    <SearchLogo />
-                ),
-            }}
+
+      <Tab.Screen
+        name="FloatingButtonScreen"
+        component={FloatingButton}
+        options={{
+          tabBarButton: () => (
+            <View style={{ flex: 1, alignItems: "center" }}>
+              <FloatingButton
+                onPress={() => navigation.navigate("AddItem")} // Navigate to your desired page
+              />
+            </View>
+          ),
+        }}
       />
-      <Tab.Screen 
-            name="Profile" 
-            component={ProfileScreen}
-            options={{
-                tabBarIcon: ({ color, size }) => (
-                    <ProfileLogo />
-                ),
-            }}
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <SearchLogo />,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <ProfileLogo />,
+        }}
       />
     </Tab.Navigator>
   );
---
+++ PINIT/screens/component/FloatingButton.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { TouchableOpacity, View, StyleSheet } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons'; // You can use FontAwesome5 for the plus icon
+
+const FloatingButton = ({ onPress }) => {
+  return (
+    <TouchableOpacity style={styles.floatingButton} onPress={onPress}>
+      <FontAwesome5 name="plus" size={24} color="white" />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  floatingButton: {
+    position: 'absolute',
+    bottom: 16,
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    backgroundColor: '#635BFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 5,
+  },
+});
+
+export default FloatingButton;
