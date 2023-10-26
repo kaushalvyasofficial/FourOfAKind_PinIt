21cde59deeaee40e04df0599816172c556f76c29
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2023-10-27 04:25:13 +0530

    added select list for add data screen

--- PINIT/screens/AddDataScreen.js
+++ PINIT/screens/AddDataScreen.js
@@ -1,10 +1,34 @@
 import React, { useEffect } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-
+import { SelectList } from 'react-native-dropdown-select-list'
+import { FontAwesome } from '@expo/vector-icons';
 const AddDataScreen = ({ navigation }) => {
+  const [selected, setSelected] = React.useState("");
+  
+  const data = [
+    {key:'1',value:'Option1'},
+    {key:'2',value:'Option2'},
+    {key:'3',value:'Option3 '},
+    {key:'4',value:'Option4'},
+  ];
+
   return (
     <View style={styles.logo}>
-      <Text>hello this is me </Text>
+      
+      
+    <SelectList 
+        setSelected={(val) => setSelected(val)} 
+        data={data} 
+        save="value"
+    /><SelectList 
+    setSelected={(val) => setSelected(val)} 
+    data={data} 
+    save="value"
+/><SelectList 
+        setSelected={(val) => setSelected(val)} 
+        data={data} 
+        save="value"
+    />
     </View>
   );
 };
