4824e4552f370a94742f776fe287097c0e3b8df9
Gourav2609 <herculeswarrior.in@gmail.com>
2023-10-27 16:04:53 +0530

    Fix02

--- PINIT/screens/LoginPage.js
+++ PINIT/screens/LoginPage.js
@@ -1,53 +1,92 @@
-import React, { useCallback, useEffect, useState} from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image, TextInput, Switch } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import Spinner from 'react-native-loading-spinner-overlay';
-import LoginLogo from '../assets/images/loginlogo';
-import {useBackHandler} from '@react-native-community/hooks'
-// import { useNavigation } from '@react-navigation/native'; // Import useNavigation
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  TextInput,
+  Switch,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import Spinner from "react-native-loading-spinner-overlay";
+import LoginLogo from "../assets/images/loginlogo";
+import { useBackHandler } from "@react-native-community/hooks";
+import AsyncStorage from "@react-native-async-storage/async-storage"; // Import AsyncStorage
 
 const LoginPage = ({ navigation }) => {
-useBackHandler(() => {
-  return true
-})
+  useBackHandler(() => {
+    return true;
+  });
 
   // const navigation = useNavigation();
-  const [selectedOption, setSelectedOption] = useState('admin'); // Default to 'admin'
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [selectedOption, setSelectedOption] = useState("admin"); // Default to 'admin'
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [rememberMe, setRememberMe] = useState(false);
+  const [rememberMe, setRememberMe] = useState(false); // New state for "Remember Me"
   const [isLoading, setIsLoading] = useState(false);
- 
+
+  useEffect(() => {
+    // Load "Remember Me" settings from AsyncStorage on component mount
+    loadRememberMeSettings();
+  }, []);
+
+  const loadRememberMeSettings = async () => {
+    try {
+      const rememberMeValue = await AsyncStorage.getItem("rememberMe");
+      if (rememberMeValue !== null) {
+        // Set the "Remember Me" state based on the value from AsyncStorage
+        setRememberMe(rememberMeValue === "true");
+        console.log('Loaded "Remember Me" settings from AsyncStorage');
+      }
+    } catch (error) {
+      console.error('Error loading "Remember Me" settings:', error);
+    }
+  };
+
+  const saveRememberMeSettings = async () => {
+    try {
+      await AsyncStorage.setItem("rememberMe", rememberMe.toString());
+    } catch (error) {
+      console.error('Error saving "Remember Me" settings:', error);
+    }
+  };
+
   const handleOptionSelect = (option) => {
     setSelectedOption(option);
-    setEmail('');
-    setPassword('');
+    setEmail("");
+    setPassword("");
     setShowPassword(false);
     setRememberMe(false);
   };
-  
-  
+
   const handleSignIn = () => {
-    setIsLoading(true); // Show loading indicator
+    setIsLoading(true);
 
     const auth = getAuth();
 
     signInWithEmailAndPassword(auth, email, password)
       .then((response) => {
-        setIsLoading(false); // Hide loading indicator
-        if (selectedOption === 'admin') {
-          navigation.navigate('Dashboard');
-        } else if (selectedOption === 'student') {
-          navigation.navigate('Dashboard');
+        setIsLoading(false);
+        if (selectedOption === "admin") {
+          navigation.navigate("Dashboard");
+        } else if (selectedOption === "student") {
+          navigation.navigate("Dashboard");
         }
-        setEmail('');
-        setPassword('');
+        setEmail("");
+        setPassword("");
+
+        // Save "Remember Me" settings when the user successfully logs in
+        if (rememberMe) {
+          saveRememberMeSettings();
+        }
+
         console.log(`${selectedOption} successfully logged in`);
       })
       .catch((error) => {
-        setIsLoading(false); // Hide loading indicator
+        setIsLoading(false);
         const errorCode = error.code;
         const errorMessage = error.message;
         alert(errorMessage);
@@ -55,7 +94,6 @@ useBackHandler(() => {
       });
   };
 
-  
   return (
     <View style={styles.container}>
       <View style={styles.pinDiv}>
@@ -63,23 +101,52 @@ useBackHandler(() => {
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity
-          style={[styles.button, styles.adminButton, selectedOption === 'admin' && styles.selectedButton, selectedOption === 'student' && styles.unselectedButton]}
-          onPress={() => handleOptionSelect('admin')}
+          style={[
+            styles.button,
+            styles.adminButton,
+            selectedOption === "admin" && styles.selectedButton,
+            selectedOption === "student" && styles.unselectedButton,
+          ]}
+          onPress={() => handleOptionSelect("admin")}
         >
-          <Text style={[styles.buttonText, selectedOption === 'student' && styles.unselectedText]}>Admin</Text>
+          <Text
+            style={[
+              styles.buttonText,
+              selectedOption === "student" && styles.unselectedText,
+            ]}
+          >
+            Admin
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity
-          style={[styles.button, styles.studentButton, selectedOption === 'student' && styles.selectedButton, selectedOption === 'admin' && styles.unselectedButton]}
-          onPress={() => handleOptionSelect('student')}
+          style={[
+            styles.button,
+            styles.studentButton,
+            selectedOption === "student" && styles.selectedButton,
+            selectedOption === "admin" && styles.unselectedButton,
+          ]}
+          onPress={() => handleOptionSelect("student")}
         >
-          <Text style={[styles.buttonText, selectedOption === 'admin' && styles.unselectedText]}>Student</Text>
+          <Text
+            style={[
+              styles.buttonText,
+              selectedOption === "admin" && styles.unselectedText,
+            ]}
+          >
+            Student
+          </Text>
         </TouchableOpacity>
       </View>
 
       {selectedOption && (
         <View style={styles.inputContainer}>
           <View style={styles.inputRow}>
-            <FontAwesome name="envelope" size={24} color="#807A7A99" style={styles.icon} />
+            <FontAwesome
+              name="envelope"
+              size={24}
+              color="#807A7A99"
+              style={styles.icon}
+            />
             <TextInput
               style={styles.input}
               placeholder="email@gmail.com"
@@ -89,7 +156,12 @@ useBackHandler(() => {
             />
           </View>
           <View style={styles.passwordContainer}>
-            <FontAwesome name="lock" size={28} color="#807A7A99" style={styles.icon} />
+            <FontAwesome
+              name="lock"
+              size={28}
+              color="#807A7A99"
+              style={styles.icon}
+            />
             <TextInput
               style={styles.passwordInput}
               placeholder="password"
@@ -102,22 +174,24 @@ useBackHandler(() => {
               style={styles.showPasswordButton}
               onPress={() => setShowPassword(!showPassword)}
             >
-              <FontAwesome name={showPassword ? 'eye-slash' : 'eye'} size={24} color="#807A7A99" paddingHorizontal={10} />
+              <FontAwesome
+                name={showPassword ? "eye-slash" : "eye"}
+                size={24}
+                color="#807A7A99"
+                paddingHorizontal={10}
+              />
             </TouchableOpacity>
           </View>
           <View style={styles.rememberMeContainer}>
             <Text>Remember Me</Text>
             <Switch
-              trackColor={{ false: '#C0C8D2', true: '#635BFF' }} // Customize the track color for false and true states
-              thumbColor={rememberMe ? '#fff' : '#fff'}
+              trackColor={{ false: "#C0C8D2", true: "#635BFF" }}
+              thumbColor={rememberMe ? "#fff" : "#fff"}
               value={rememberMe}
               onValueChange={(value) => setRememberMe(value)}
             />
           </View>
-          <TouchableOpacity
-            style={styles.loginButton}
-            onPress={handleSignIn}
-          >
+          <TouchableOpacity style={styles.loginButton} onPress={handleSignIn}>
             <Text style={styles.loginButtonText}>SIGN IN</Text>
           </TouchableOpacity>
         </View>
@@ -126,8 +200,9 @@ useBackHandler(() => {
       {isLoading && (
         <Spinner
           visible={isLoading}
-          textContent={'Signing In...'}
-          textStyle={styles.spinnerText} />
+          textContent={"Signing In..."}
+          textStyle={styles.spinnerText}
+        />
       )}
     </View>
   );
@@ -136,66 +211,65 @@ useBackHandler(() => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
   },
   pinDiv: {
     flex: 1,
-    justifyContent: 'center',
-    marginTop: '30%',
+    justifyContent: "center",
+    marginTop: "30%",
     // backgroundColor: '#000',
   },
   pinImage: {
     // padding: 10,
   },
   buttonContainer: {
-    flexDirection: 'row',
-    // flex: 1, 
-    justifyContent: 'center',
-    width: '70%',
+    flexDirection: "row",
+    // flex: 1,
+    justifyContent: "center",
+    width: "70%",
     marginBottom: 40,
   },
   button: {
     flex: 1,
     height: 60,
     // borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderBottomWidth: 2,
-    borderColor: '#A3ACBA'
+    borderColor: "#A3ACBA",
     // marginBottom: 20,
   },
   selectedButton: {
     borderBottomWidth: 2, // Add underline
-    borderColor: '#635BFF', // Color of underline
+    borderColor: "#635BFF", // Color of underline
   },
   unselectedText: {
-    color: '#A3ACBA',
+    color: "#A3ACBA",
     // borderBottomWidth: 2,
-    borderColor: '#000',
+    borderColor: "#000",
     // backgroundColor: '#000',
   },
   unselectedButton: {
     // backgroundColor: '#000',
     borderBottomWidth: 2, // Add underline
-    borderColor: '#dddddd',
-
+    borderColor: "#dddddd",
   },
   buttonText: {
     fontSize: 24,
     // fontWeight: 'bold',
-    color: '#000',
+    color: "#000",
   },
   inputContainer: {
     flex: 3,
-    width: '80%',
+    width: "80%",
   },
   inputRow: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     borderWidth: 1,
-    borderColor: '#E4DFDF',
+    borderColor: "#E4DFDF",
     borderRadius: 16,
     marginBottom: 20,
   },
@@ -208,10 +282,10 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   passwordContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     borderWidth: 1,
-    borderColor: '#E4DFDF',
+    borderColor: "#E4DFDF",
     borderRadius: 16,
     marginBottom: 10,
   },
@@ -224,25 +298,25 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   rememberMeContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   loginButton: {
-    backgroundColor: '#635BFF',
+    backgroundColor: "#635BFF",
     height: 60,
     borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 10,
   },
   loginButtonText: {
     fontSize: 24,
     // fontWeight: 'bold',
-    color: '#fff',
+    color: "#fff",
   },
   spinnerText: {
-    color: '#FFF',
-  }
+    color: "#FFF",
+  },
 });
 
 export default LoginPage;
